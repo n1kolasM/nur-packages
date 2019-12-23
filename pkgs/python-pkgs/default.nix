113c527b774911f9bebe1fbed5ea04a151520142
Nikolay Marchuk <marchuk.nikolay.a@gmail.com>
2019-12-23 23:10:47 +0700

    Add flake8-annotations-complexity

--- pkgs/python-pkgs/default.nix
+++ pkgs/python-pkgs/default.nix
@@ -5,4 +5,5 @@ rec {
   stevedore = callPackage ./stevedore {};
   bandit = callPackage ./bandit { inherit stevedore; };
   darglint = callPackage ./darglint {};
+  flake8-annotations-complexity = callPackage ./flake8-annotations-complexity {};
 }
