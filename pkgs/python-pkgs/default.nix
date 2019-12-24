18fcc9c521e4250624ece340678cdee6d8295d27
Nikolay Marchuk <marchuk.nikolay.a@gmail.com>
2019-12-24 20:02:53 +0700

    Add flake8-broken-line

--- pkgs/python-pkgs/default.nix
+++ pkgs/python-pkgs/default.nix
@@ -1,10 +1,16 @@
-{ callPackage }:
+{ callPackage, poetry }:
 
-rec {
+let
+  isPackageBroken = package: builtins.hasAttr "broken" package.meta && package.meta.broken;
+in rec {
   cognitive-complexity = callPackage ./cognitive-complexity {};
   stevedore = callPackage ./stevedore {};
   bandit = callPackage ./bandit { inherit stevedore; };
   darglint = callPackage ./darglint {};
   flake8-annotations-complexity = callPackage ./flake8-annotations-complexity {};
   flake8-bandit = callPackage ./flake8-bandit { inherit bandit; };
+  flake8-broken-line = if !isPackageBroken poetry then
+    callPackage ./flake8-broken-line {}
+  else
+    callPackage ./flake8-broken-line/without-poetry.nix {};
 }
